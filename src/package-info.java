eb9bce8fd7fa3d64e20b9a1b22492c17213633d1
Esteban Moya <moyamena@gmail.com>
2024-08-16 18:53:55 -0400

    Terminado

---
+++ src/package-info.java
@@ -0,0 +1,10 @@
+package ReciclaJeans;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
